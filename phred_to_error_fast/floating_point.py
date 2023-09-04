287f4525f91d9cc5b8490552c63fdd6852226beb
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-09-04 21:47:08 +0200

    Check floating point representation

---
+++ phred_to_error_fast/floating_point.py
@@ -0,0 +1,70 @@
+import struct
+
+SIGN_SHIFT = 63
+SIGN_MASK = 1 << SIGN_SHIFT
+EXPONENT_SHIFT = 52
+EXPONENT_MASK = 0b111_1111_1111 << EXPONENT_SHIFT
+SIGNIFICAND_MASK = (1 << EXPONENT_SHIFT) - 1
+
+
+def cast_float_to_int(f: float):
+    float_bytes = struct.pack("d", f)
+    i, = struct.unpack("Q", float_bytes)
+    return i
+
+
+def cast_int_to_float(i: int):
+    int_bytes = struct.pack("Q", i)
+    f, = struct.unpack("d", int_bytes)
+    return f
+
+
+class Double:
+    sign: bool
+    exponent: int
+    significand: int
+
+    def __init__(self, sign, exponent: int, significand: int):
+        self.sign = bool(sign)
+        self.exponent = exponent
+        self.significand = significand
+
+    def significand_fraction(self):
+        sig_bits = self.significand
+        accumulator = 1.0
+        for i in range(52, 0 , -1):
+            bit = sig_bits & 1
+            sig_bits >>= 1
+            accumulator += bit * 2 ** -i
+        return accumulator
+
+    @classmethod
+    def from_int(cls, i: int):
+        return cls(
+            sign=i & SIGN_MASK,
+            exponent=(i & EXPONENT_MASK) >> EXPONENT_SHIFT,
+            significand=i & SIGNIFICAND_MASK)
+
+    @classmethod
+    def from_float(cls, f: float):
+        return cls.from_int(cast_float_to_int(f))
+
+    def __int__(self):
+        return (self.significand |
+                (self.exponent << EXPONENT_SHIFT) |
+                (self.sign << SIGN_SHIFT))
+
+    def __float__(self):
+        return cast_int_to_float(int(self))
+
+    def __repr__(self):
+        return (f"Double(sign={self.sign}, "
+                f"exponent={self.exponent}, "
+                f"significand={self.significand})")
+
+
+if __name__ == "__main__":
+    errors = [10 ** (i / -10) for i in range(94)]
+    doubles = [Double.from_float(e) for e in errors]
+    for i, d in enumerate(doubles):
+        print(f"{i:2}\t{repr(d):80}{d.significand_fraction()}\t{bin(int(d))}")
